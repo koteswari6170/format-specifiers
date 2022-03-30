9b3e0295f02d4acc1587bccbd5396dd799fcdf99
koteswari6170 <102402089+koteswari6170@users.noreply.github.com>
2022-03-30 11:28:27 +0530

    Add files via upload

---
+++ formatspec.py
@@ -0,0 +1,62 @@
+# a=7
+# print(a,type(a)) #7 int
+# b=float(a)
+# print(b,type(b)) # 7.0 float
+# c=str(a)
+# print(c,type(c)) #7 str
+# x=7
+# print("the value of x is",x) #the value of x is 7
+# print("the value of x is%d"%x) #the value of x is7
+# print("the value of x is"+' '+str(x)) #the value of x is 7
+# print("the value of x is"+' '+x) #typeerror
+"""convert float to int and str"""
+# a=5.6
+# print(a,type(a)) #5.6 float
+# b=int(a)
+# print(b,type(b)) #5 int
+# c=str(a)
+# print(c,type(c)) #5.6 str
+"""converting str to int and float"""
+# t="ameerpet"
+# print(t,type(t)) #ameerpet str
+# y=int(t) #valueerror
+# z=float(t) #valueerror
+# p='134.6'
+# print(p,type(p)) #134.6 str
+# s=float(p)
+# print(s,type(s)) #134.6 float
+# r=int(s)
+# print(r,type(r)) #134 int
+# a=5+4
+# print(a,type(a)) #9 int
+# b=0.1+2.1
+# print(b,type(b)) #2.2 float
+# print("%f"%b) #2.200000
+# c="python programming"
+# print(c,type(c)) #python programming str
+# y='josh'; j='hai'
+# print(y,type(y)) #josh str
+# print("%s"%y) #hai
+# k=57
+# print("%c"%k) #9
+# e='Y'
+# print("%c"%e) #Y
+# print(chr(0)+"python") #python
+# print(chr(32)+"python") # python
+# a=5
+# b=float(a)
+# print(b) #5.0
+# c=6.7
+# d=int(c)
+# print(d) #6
+# e='josh'
+# f=int(e) #valuerror
+# g=float(e)#valueerror
+# h='45'
+# j=float(h)
+# print(j) #45.0
+# k=int(j)
+# print(k) #45
+p=6;r=6.7
+s=p+r
+print(s,type(s)) #66.7
